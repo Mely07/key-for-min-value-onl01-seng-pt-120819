b5642ad0217286a06eb6061f95bea6423f0d7777
Damely Tineo <damely.tineo07@gmail.com>
2019-12-21 06:29:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,10 +8,14 @@ def key_for_min_value(name_hash)
       return nil 
   else
     min = name_hash.values[0]
+    min_key = name_hash.keys[0]
     name_hash.each do |key, value|
       if value < min
         min = value 
+      
+    
       end
+      
     return minumum
   end
 
